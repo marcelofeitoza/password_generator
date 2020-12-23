dc602e9d8519f5a303eaf658669abb917f77e5b1
Marcelo G Feitoza <71825192+marcelofeitoza@users.noreply.github.com>
2020-12-23 11:44:00 -0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,13 +1,13 @@
 import random
 
-chars= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*()"     
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456!£$%^&*(`)"
 
-while 1:
-    password_len = int(input("Password length: "))                            
-    password_count = int(input("How many passwords do you want: "))      
-    for x in range(0, password_count):
-        password = ""
+while True:
+    password_len = int(input("Password length?: "))
+    password_count = int(input("How many passwords?: "))
+    for x in range(0,password_count):
+        password  = ""
         for x in range(0,password_len):
-            password_char = random.choice(chars)                         
-            password = password + password_char                               
-        print("Here is your password: ", password)                                   
+            password_char = random.choice(chars)
+            password      = password + password_char
+        print("Here is your password : ", password)
